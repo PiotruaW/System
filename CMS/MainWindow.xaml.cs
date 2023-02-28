d7eb1a1093bc48bc76d774cadd5535884059841e
Mafeuszkaczor <mafeuszkaczor@gmail.com>
2023-02-28 16:23:53 +0100

    Trappin na fula

--- CMS/MainWindow.xaml.cs
+++ CMS/MainWindow.xaml.cs
@@ -21,5 +21,24 @@ namespace CMS
                 this.DragMove();
             }
         }
+        private void ExitButton(object sender, MouseButtonEventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+        private void MinButton(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void MaxButton(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
     }
 }
