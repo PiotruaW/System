2eea266bb259c73d4244a74482d49d69056e775e
ppiasta <ppiasta@WA2001LN0731>
2023-03-01 09:04:50 +0100

    test

--- CMS/MVVM/Model/AddModel.cs
+++ CMS/MVVM/Model/AddModel.cs
@@ -1,8 +1,13 @@
 ﻿using desktop_translator.Core;
+using System.Windows;
 
 namespace CMS.MVVM.Model
 {
     class AddModel : ObservableObject
     {
+        public void Test()
+        {
+            MessageBox.Show("test");
+        }
     }
 }
